daed96bb2ec3ddc91b4b1336e641d23b9f6a2955
IvanS <ivan443931@gmail.com>
2023-06-24 16:18:06 +0300

    Config changes, empty function

---
+++ src/AzureFunctions/SolutionTwo.ProductForceReleaseFunctionApp/ProductForceRelease.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace SolutionTwo.ProductForceReleaseFunctionApp;
+
+public static class ProductForceRelease
+{
+    [FunctionName("ProductForceRelease")]
+    public static async Task RunAsync([TimerTrigger("0 0 3 * * *")] TimerInfo myTimer, ILogger log)
+    {
+        log.LogInformation($"C# Timer trigger function executed at: {DateTime.UtcNow}");
+        
+    }
+}
